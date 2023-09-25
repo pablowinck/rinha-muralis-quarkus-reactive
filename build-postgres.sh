43b4f6019786511f1320050f3bc4b382a38e5356
pablowinck <contato@pablowinter.com.br>
2023-09-24 23:15:34 -0300

    v: 2.2

--- build-postgres.sh
+++ build-postgres.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-docker build -t pablowinck/custom-postgres:1.0 -f Dockerfile.postgres . && docker push pablowinck/custom-postgres:1.0
+docker build -t pablowinck/custom-postgres:1.1 -f Dockerfile.postgres . && docker push pablowinck/custom-postgres:1.1
