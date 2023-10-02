8aab083cc35d7900392a2f92c9f80c12a2961e20
pablowinck <contato@pablowinter.com.br>
2023-10-02 09:14:19 -0300

    fix bug

--- src/main/java/br/com/pablowinter/Pessoa.java
+++ src/main/java/br/com/pablowinter/Pessoa.java
@@ -16,12 +16,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 
-
 @Entity
-@Table(name = "pessoas",
-        indexes = {
-                @Index(name = "idx_pessoas_term", columnList = "term")
-        })
+@Table(name = "pessoas", indexes = {
+        @Index(name = "idx_pessoas_term", columnList = "term")
+})
 @Cacheable
 public class Pessoa extends PanacheEntityBase {
 
@@ -52,14 +50,18 @@ public class Pessoa extends PanacheEntityBase {
 
     public void prepareToPersist() {
         this.id = UUID.randomUUID().toString();
-        this.term = this.nome + this.apelido;
-        if (this.stack != null) this.term += String.join("", this.stack);
-        if (term.length() >= 255) term = term.substring(0, 254);
+        this.term = this.nome + "_" + this.apelido + "_";
+        if (this.stack != null)
+            this.term += String.join("_", this.stack);
+        if (term.length() >= 255)
+            term = term.substring(0, 254);
         // fix date format if necessary (like 2001-8-14 -> 2001-08-14)
         if (this.nascimento.length() < 10) {
             String[] split = this.nascimento.split("-");
-            if (split[1].length() == 1) split[1] = "0" + split[1];
-            if (split[2].length() == 1) split[2] = "0" + split[2];
+            if (split[1].length() == 1)
+                split[1] = "0" + split[1];
+            if (split[2].length() == 1)
+                split[2] = "0" + split[2];
             this.nascimento = String.join("-", split);
         }
     }
@@ -107,27 +109,33 @@ public class Pessoa extends PanacheEntityBase {
     @JsonIgnore
     public boolean isUnprossessableEntity() {
         boolean outOfRange = (this.nome.length() > 100 && !this.nome.endsWith("🏖")) || this.apelido.length() > 32;
-        if (outOfRange) return true;
-        if (hasNullInStack()) return true;
-        if (hasStackWithMoreThan32Characters()) return true;
+        if (outOfRange)
+            return true;
+        if (hasNullInStack())
+            return true;
+        if (hasStackWithMoreThan32Characters())
+            return true;
         return isNascimentoInvalido();
     }
 
     @JsonIgnore
     public boolean isBadRequest() {
         var isNascimentoEmpty = this.nascimento != null && this.nascimento.isBlank();
-        if (isNascimentoEmpty) return true;
+        if (isNascimentoEmpty)
+            return true;
         var isNascimentoTypeInvalid = this.nascimento != null && this.nascimento.split("-").length != 3;
         return isNascimentoTypeInvalid;
     }
 
     private boolean hasNullInStack() {
-        if (stack == null) return false;
+        if (stack == null)
+            return false;
         return stack.stream().anyMatch(Objects::isNull);
     }
 
     private boolean hasStackWithMoreThan32Characters() {
-        if (stack == null) return false;
+        if (stack == null)
+            return false;
         return stack.stream().anyMatch(s -> s != null && s.length() > 32);
     }
 
@@ -137,14 +145,22 @@ public class Pessoa extends PanacheEntityBase {
         int mes = Integer.parseInt(split[1]);
         int dia = Integer.parseInt(split[2]);
         Calendar calendar = Calendar.getInstance();
-        if (ano < 1 || ano > calendar.get(Calendar.YEAR)) return true;
-        if (mes < 1 || mes > 12) return true;
-        if (dia < 1 || dia > 31) return true;
-        if (mes == 2 && dia > 29) return true;
-        if (mes == 4 && dia > 30) return true;
-        if (mes == 6 && dia > 30) return true;
-        if (mes == 9 && dia > 30) return true;
-        if (!yearFebruaryHas29Days(ano) && mes == 2 && dia > 28) return true;
+        if (ano < 1 || ano > calendar.get(Calendar.YEAR))
+            return true;
+        if (mes < 1 || mes > 12)
+            return true;
+        if (dia < 1 || dia > 31)
+            return true;
+        if (mes == 2 && dia > 29)
+            return true;
+        if (mes == 4 && dia > 30)
+            return true;
+        if (mes == 6 && dia > 30)
+            return true;
+        if (mes == 9 && dia > 30)
+            return true;
+        if (!yearFebruaryHas29Days(ano) && mes == 2 && dia > 28)
+            return true;
         return mes == 11 && dia > 30;
     }
 
