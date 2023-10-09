34c4ce6500a12828f3791114cd64301707c70e65
pablowinck <pablowinck123@gmail.com>
2023-10-09 17:40:59 -0300

    exclui arquivos nao utilizados

--- sobe-docker-compose.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-IAM_USER_KEY="/home/pablo/Documents/rinhabackend-ec2-keypair.pem"
-SERVER="ubuntu@ec2-54-89-252-232.compute-1.amazonaws.com"
-
-echo "Copying compose to server"
-scp -i $IAM_USER_KEY docker-compose.yml $SERVER:/home/ubuntu || echo "failed to copy compose"
